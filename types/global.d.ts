27c9a9ef10bc4cefdf6e6db01cb86d32c255b9bc
Jan Bobisud <me@bobisjan.com>
2023-10-20 16:55:32 +0200

    Ember Types 🎉

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,11 +1,3 @@
-// Types for compiled templates
-declare module 'autoskola-beran/templates/*' {
-  import { TemplateFactory } from 'ember-cli-htmlbars';
-
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
-
 declare module '*.jpg' {
   const path: string;
   export default path;
